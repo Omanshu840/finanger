dfd09f0915b027c1bd86235286e36c552b515b54
Omanshu Mahawar <omanshumahawar.181co237@nitk.edu.in>
2025-10-02 14:18:07 +0530

    Updates

--- assets/mail-ayLiECN-.js
+++ assets/mail-ayLiECN-.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-MHHjkg74.js";/**
+import{c as e}from"./index-Bg4i3uS9.js";/**
  * @license lucide-react v0.544.0 - ISC
  *
  * This source code is licensed under the ISC license.
