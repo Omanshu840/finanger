30c66644d10c6fa5251313ef035307b47b83d1db
Omanshu Mahawar <omanshumahawar.181co237@nitk.edu.in>
2025-10-02 16:13:52 +0530

    Updates

--- assets/mail-DQt94Mam.js
+++ assets/mail-DQt94Mam.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-BDqeGaYb.js";/**
+import{c as e}from"./index-D6PnLut5.js";/**
  * @license lucide-react v0.544.0 - ISC
  *
  * This source code is licensed under the ISC license.
